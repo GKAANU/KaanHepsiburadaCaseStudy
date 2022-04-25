575deecc02b80805ccab5dae23475ab1f6abd9d3
GKAANU <kaan.uzun.61@outlook.com>
2022-04-25 15:12:29 +0300

    ..

--- src/test/java/com/Hepsiburada/Runners/FailedTestRunner.java
+++ src/test/java/com/Hepsiburada/Runners/FailedTestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = "html:target/failed-html-reports",
         features = "@target/rerun.txt",
-        glue = "com/Heetch/StepDefinitions"
+        glue = "com/Hepsiburada/StepDefinitions"
 )
 public class FailedTestRunner {
 }
